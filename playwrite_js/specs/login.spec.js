6761549f48c77f3939fb96a732892e2ae5afe13f
Peter Burch <burchpw@gmail.com>
2026-09-08 20:11:11 -0500

    Refactor Login spec
    Added Fixtures for Page Object Pages

--- playwrite_js/specs/login.spec.js
+++ playwrite_js/specs/login.spec.js
@@ -3,60 +3,59 @@ import { LogInHelper } from '../helpers/log_in_helper.js';
 import { LoginPage } from '../page_objects/login_page.js';
 import { LogoutPage } from '../page_objects/logout_page.js';
 
-// Fixture to handle loading credentials
+// Fixture to handle loading credentials and load page objects
 const test = baseTest.extend({
     credentials: async ( { }, use) => {
         const login_credentials = new LogInHelper();
         await login_credentials.init();
         await use(login_credentials);
+    },
+    loginPage: async ( { page }, use) => {
+        const login_page = new LoginPage(page);
+        await use(login_page);
+    },
+    logoutPage: async ( { page }, use ) => {
+        const logout_page = new LogoutPage(page);
+        await use(logout_page);
     }
 });
 
-test.describe('Login Tests', () => {
+test.describe('Login Page', () => {
 
-    test('logs the user in', async ({ page, credentials }) => { 
-        const login_page = new LoginPage(page);
-        const logout_page = new LogoutPage(page);
+    test('logs the user in', async ({ credentials, loginPage, logoutPage }) => { 
+        await loginPage.goto();
 
-        await login_page.goto();
+        await loginPage.user_name_field().fill(credentials.user_name());
+        await loginPage.password_field().fill(credentials.password());
+        await loginPage.submit_button().click();
 
-        await login_page.user_name_field().fill(credentials.user_name());
-        await login_page.password_field().fill(credentials.password());
-        await login_page.submit_button().click();
-
-        await expect(logout_page.log_out_button()).toBeVisible();
+        await expect(logoutPage.log_out_button()).toBeVisible();
     });
 
-    test('handles incorrect user_name', async ({ page, credentials }) => { 
-        const login_page = new LoginPage(page);
+    test('handles incorrect user_name', async ({ credentials, loginPage }) => { 
+        await loginPage.goto();
 
-        await login_page.goto();
+        await loginPage.user_name_field().fill("incorrectUser");
+        await loginPage.password_field().fill(credentials.password());
+        await loginPage.submit_button().click();
 
-        await login_page.user_name_field().fill("incorrectUser");
-        await login_page.password_field().fill(credentials.password());
-        await login_page.submit_button().click();
-
-        await expect(login_page.error_message()).toHaveText('Your username is invalid!')
+        await expect(loginPage.error_message()).toHaveText('Your username is invalid!')
     });
 
-    test('handles incorrect password', async ({ page, credentials }) => { 
-        const login_page = new LoginPage(page);
+    test('handles incorrect password', async ({ credentials, loginPage }) => { 
+        await loginPage.goto();
 
-        await login_page.goto();
+        await loginPage.user_name_field().fill(credentials.user_name());
+        await loginPage.password_field().fill('incorrectPassword');
+        await loginPage.submit_button().click();
 
-        await login_page.user_name_field().fill(credentials.user_name());
-        await login_page.password_field().fill('incorrectPassword');
-        await login_page.submit_button().click();
-
-        await expect(login_page.error_message()).toHaveText('Your password is invalid!')
+        await expect(loginPage.error_message()).toHaveText('Your password is invalid!')
     });
 
-    test('handles blank form submission', async ({ page }) => { 
-        const login_page = new LoginPage(page);
+    test('handles blank form submission', async ({ loginPage }) => { 
+        await loginPage.goto();
 
-        await login_page.goto();
-
-        await login_page.submit_button().click();
-        await expect(login_page.error_message()).toHaveText('Your username is invalid!')
+        await loginPage.submit_button().click();
+        await expect(loginPage.error_message()).toHaveText('Your username is invalid!')
     });
 });
